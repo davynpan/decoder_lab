0c6213b38c9ec1f937fa21cc9d9e73d95327b9e4
Davyn Pan <pan.davyn@ufl.edu>
2023-03-21 13:21:44 -0400

    added comments

--- decoder_lab.py
+++ decoder_lab.py
@@ -1,12 +1,12 @@
-def encode(string):
+def encode(string):         #Code by Davyn Pan
     new_password = ""
     for i in string:
-        new_password = new_password + f"{i + 3}"
+        new_password = new_password + f"{i + 3}"            #adds 3 to every integer in password
     return new_password
 
 
 def decode(string):
-    decoded_password = ""
+    decoded_password = ""                   #subtracts 3 from all integers of password
     for i in string:
         decoded_password = decoded_password + f"{i - 3}"
     return decoded_password
@@ -23,7 +23,7 @@ while __name__ == "__main__":
     if menu_option == 1:
         orig_password = input("Please enter your password to encode: ")
         encoded_password = encode(orig_password)
-        print("Your password has been encoded and stored!")
+        print("Your password has been encoded and stored!")         #stores encoded password as encoded_password
 
     if menu_option == 2:
         print(f"The encoded password is {encoded_password}, and the original password is {orig_password}.")
